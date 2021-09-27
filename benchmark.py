8e53cc1946332363b3c9b6b2ec1208a9084675f7
ZongSingHuang <a26833765@gmail.com>
2021-09-27 22:32:24 +0800

    1

--- benchmark.py
+++ benchmark.py
@@ -9,35 +9,30 @@ import numpy as np
 import pandas as pd
 
 def makespan_3(X, M, N):
+    X = X.astype(int)
     Process_Time = np.array([[8.00, 10.0, 5.00],
                              [15.0, 9.00, 12.0],
                              [9.00, 7.00, 19.0]])
+    Process_Time = pd.DataFrame(data=Process_Time, columns=['Operation 1', 'Operation 2', 'Operation 3'], index=['Job 1', 'Job 2', 'Job 3'], dtype=int)
 
     P = X.shape[0]
     
     for i in range(P):
-        CWH = np.zeros([N]) # 累計工時 Cumulative working hours
-        PH = np.zeros([N, M]) - 1 # 加工履歷 Processing history
-        O = np.zeros([N]) # 加工次數
+        CWH_machine = pd.DataFrame(data=np.zeros([N]), columns=['time'], index=['machine 1', 'machine 2', 'machine 3'], dtype=int) # 累計工時 Cumulative working hours
+        CWH_job = pd.DataFrame(data=np.zeros([N]), columns=['time'], index=['job 1', 'job 2', 'job 3'], dtype=int) # 累計工時 Cumulative working hours
+        PH = pd.DataFrame(data=np.zeros([N, M])-1, columns=['Operation 1', 'Operation 2', 'Operation 3'], index=['Job 1', 'Job 2', 'Job 3'], dtype=int) # 加工履歷 Processing history
+        O = pd.DataFrame(np.zeros([N]), index=['job 1', 'job 2', 'job 3'], dtype=int) # 加工次數
         print('初始化 累計工時、加工履歷')
         print('當前候選解為 {X}'.format(X=X[i]))
         
         for job in X[i]:
-            sorted_CWH = CWH.argsort()
-            print('目前各機台的累計工時，由小到大依序為')
-            print('編號 {idx}'.format(idx=sorted_CWH))
-            print('工時 {hr}'.format(hr=np.sort(CWH)))
+            CWH_machine = CWH_machine.sort_values(by=['time'])
             
-            for machine in sorted_CWH:
-                print('判定 工件 {job} 是否曾在 機台 {machine} 進行加工'.format(job=job, machine=machine))
-                print('機台 {machine} 當前的履歷為 {PH}'.format(machine=machine, PH=PH[machine]))
-                if machine not in PH[int(job)]:
-                    CWH[int(job)] += Process_Time[int(job), int(O[int(job)])]
-                    PH[int(job), int(O[int(job)])] = machine
-                    O[int(job)] += 1
-                    print('當前的加工履歷為 \n{PH}'.format(PH=PH))
-                    print('當前的累計工時為 {CWH}'.format(CWH=CWH))
-                    print('當前的加工次數為 {O}'.format(O=O))
+            for idx, machine in enumerate(CWH_machine.index):
+                if idx not in PH.iloc[job, :]:
+                    CWH_machine.iloc[idx] += Process_Time.iloc[job, O.iloc[job]].values
+                    PH.iloc[job, O.iloc[job]] = int(machine[-1])-1
+                    O.iloc[job] += 1
                     break
     
         PH = pd.DataFrame(data=PH.astype(int)+1, columns=['Operation 1', 'Operation 2', 'Operation 3'], index=['Job 1', 'Job 2', 'Job 3'])
